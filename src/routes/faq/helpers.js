ca82cae5ae68e3d2ffaf2f69df320540d54b9a99
Oleg Zaskovets <olegzask@gmail.com>
2022-11-30 23:14:32 -0600

    updt

--- src/routes/faq/helpers.js
+++ src/routes/faq/helpers.js
@@ -1,7 +1,10 @@
 export const answers = {
   firstQuestion:
-    "No, your vehicle is not 'bullet proof'. Film is designed to act as a sacrificial layer between elements and your car's paint. Film might get damaged especially when encountring a large rock on highway speeds.",
-  secondQuestion: "Answer for 2nd question",
-  thirdQuestion: "Answer for 3rd question",
-  fourthQuestion: "Answer for 4th question",
+    "There are many things that can have an effect on price. But on average sedans are $280+. SUVs are $260+. MiniVans are $300+. Trucks are $200+",
+  secondQuestion:
+    "It really denepds on what vehicle needs to be wraped. Complexity & material amount differs from vehicle to vehicle. Its always better to contact us to get a quote.",
+  thirdQuestion:
+    "The Film is designed to be a sacrificial layer that takes all the beating & damage. It does not give you 100% guarantee that you will never get a paint damage. A big enough, sharp rock can still rip a hole in the film & damage the paint. But in most cases PPF will absorb the impact and leave your paint undamaged.",
+  fourthQuestion:
+    "Dyed window tint films give you 99% UV Rejection, privacy & some heat rejection. Ceramic films on other hand have all the abilities of regular dyed film but have a very high heat rejection compared to dyed films. So if your target is to keep your car cooler, ceramic film is your choice.",
 };
